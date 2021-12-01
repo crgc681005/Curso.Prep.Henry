a261eb2cba3201edac53251aebfe0f23182214c5
crgc681005 <crgc681005@hotmail.com>
2021-11-30 20:56:30 -0500

    first commit 20211130

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'Hola Carlos Guzman';
+const nuevaString = 'Hola 20111130';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 1;
